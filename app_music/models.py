0038f59c283eeae7fa14591e51f3141c9641d496
QBIC <sksnslsk@gmail.com>
2022-07-12 23:32:44 +0900

    [fix] models bug fix, footer update

--- app_music/models.py
+++ app_music/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class Diary(models.model):
+class Diary(models.Model):
     user = models.CharField(max_length=15)
     title = models.CharField(max_length=63)
     create_datetime = models.DateTimeField(auto_now_add=True)
